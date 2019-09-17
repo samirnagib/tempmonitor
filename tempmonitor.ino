e34c3f569d01d21342c61d6af1d81f47a64bf016
Samir Nagib <samir.nagib@gmail.com>
2019-09-17 14:30:15 -0300

    Ajustes no layout

--- tempmonitor.ino
+++ tempmonitor.ino
@@ -8,7 +8,7 @@ void setup() {
     M5.begin();
     Wire.begin();
     M5.Lcd.setBrightness(30);
-    M5.Lcd.setCursor(0, 5);
+    M5.Lcd.setCursor(0, 0);
 
 }
 
@@ -24,12 +24,16 @@ void loop() {
     M5.Lcd.printf("%2.1f", tmp);
     M5.Lcd.setTextColor(WHITE, BLACK);
     M5.Lcd.printf(" C\r\n");
+    M5.Lcd.println("");
     M5.Lcd.print("Hum : ");
     M5.Lcd.setTextColor(GREEN, BLACK);
     M5.Lcd.printf("%2.1f", hum);
     M5.Lcd.setTextColor(WHITE, BLACK);
     M5.Lcd.printf(" %%\n");
-
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setCursor(10, 210);
+    M5.Lcd.println("Samir Lins Nagib");
+    M5.Lcd.drawRect(5,200,300,30,RED);
     delay(15000);
 
 }
